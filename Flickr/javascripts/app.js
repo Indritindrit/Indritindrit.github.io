3bd9fefcc5fb80a90702eb349eb772d536589dcf
Indrit Disha <indrit.disha.mail@gmail.com>
2018-07-24 21:21:50 +0200

    detaje

--- Flickr/javascripts/app.js
+++ Flickr/javascripts/app.js
@@ -1,6 +1,6 @@
 var main=function () {
 	"use strict";
-	 var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
+	 var url = "https://api.flickr.com/services/feeds/photos_public.gne?" +
                 "tags=dogs&format=json&jsoncallback=?";
 
   $.getJSON(url, function (flickrResponse) {
